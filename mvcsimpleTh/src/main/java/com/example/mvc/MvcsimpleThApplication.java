d3669e9bbe02e6900ce7298a87c965bcf0ae544c
Maria Bazan <mariabazan@gmail.com>
2023-03-29 18:52:20 +0200

    Proyecto de Spring MVC que devuelve una pagina web

---
+++ mvcsimpleTh/src/main/java/com/example/mvc/MvcsimpleThApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcsimpleThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcsimpleThApplication.class, args);
+	}
+
+}
---
+++ mvcsimpleTh/src/main/java/com/example/mvc/controller/HolaMundoController.java
@@ -0,0 +1,19 @@
+package com.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hola")
+public class HolaMundoController {
+	
+	
+	
+	@GetMapping
+	public String saludo() {
+		return "pagina";
+	}
+
+}
