ec4258c9af5f50debfbc53b771d7d7b9d6dd380a
Mañanas <Maanas@host.docker.internal>
2023-03-30 14:02:40 +0200

    cambio xx

--- mvcsimpleThEnlaces/src/main/java/com/example/mvc/MvcsimpleThEnlacesApplication.java
+++ mvcsimpleThEnlaces/src/main/java/com/example/mvc/MvcsimpleThEnlacesApplication.java
@@ -3,7 +3,7 @@ package com.example.mvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class MvcsimpleThEnlacesApplication {
 
 	public static void main(String[] args) {
