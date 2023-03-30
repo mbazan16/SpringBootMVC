1e68cda7ee80849a723c1376f2ca35471ad2d23f
Letspr <lparrondo@gmail.com>
2023-03-30 20:38:09 +0200

    Prueba casa

--- ejemplo_jsp/src/main/java/com/ejemplo/controller/InicioController.java
+++ ejemplo_jsp/src/main/java/com/ejemplo/controller/InicioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
-public class InicioController {
+public class  InicioController {
 	Logger log = LoggerFactory.getLogger(InicioController.class);
 	
 	@GetMapping
