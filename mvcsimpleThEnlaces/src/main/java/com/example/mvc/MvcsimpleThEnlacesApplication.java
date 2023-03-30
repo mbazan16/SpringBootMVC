9d8a1579ba3e29ffe76bdd5ba320e4741e2da9ec
Mañanas <Maanas@host.docker.internal>
2023-03-30 14:01:48 +0200

    Prueba commit

--- mvcsimpleThEnlaces/src/main/java/com/example/mvc/MvcsimpleThEnlacesApplication.java
+++ mvcsimpleThEnlaces/src/main/java/com/example/mvc/MvcsimpleThEnlacesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MvcsimpleThEnlacesApplication {
 
-	public static void main(String[] args) {
+	public static void  main(String[] args) {
 		SpringApplication.run(MvcsimpleThEnlacesApplication.class, args);
 	}
 
