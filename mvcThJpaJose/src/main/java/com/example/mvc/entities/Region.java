c8099337404a717e772a454d140307cd1bf81f38
Antonio J. Turel <antonioturel@gmail.com>
2023-03-31 13:47:53 +0200

    proyecto spring mvc Jose

---
+++ mvcThJpaJose/src/main/java/com/example/mvc/entities/Region.java
@@ -0,0 +1,37 @@
+package com.example.mvc.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="REGIONS")
+public class Region {
+
+	@Id
+	@Column(name="REGION_ID")
+	private Integer id;
+	@Column(name="REGION_NAME")
+	private String nombre;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Override
+	public String toString() {
+		return "Region [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+}
