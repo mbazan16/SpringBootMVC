d61b53a3b2cd4fd2ea8d87c27b162253977ad8d3
Mañanas <Maanas@host.docker.internal>
2023-03-31 13:47:56 +0200

    Proyecto SpringMVCJPAMiguel

---
+++ mvcSimpleThJPAMiguel/src/main/java/com/example/mvc/repositories/RegionRepository.java
@@ -0,0 +1,9 @@
+package com.example.mvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.mvc.entities.Region;
+
+public interface RegionRepository extends JpaRepository<Region,Integer> {
+
+}
---
+++ mvcSimpleThJPAMiguel/src/test/java/com/example/mvc/MvcSimpleThJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSimpleThJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
