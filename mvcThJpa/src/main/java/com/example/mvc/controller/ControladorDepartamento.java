4b82581d800bf54bbf467c2ab93aaa7498bb4b64
Mañanas <Maanas@host.docker.internal>
2023-04-14 10:34:00 +0200

    cambios 14/04

--- mvcThJpa/src/main/java/com/example/mvc/controller/ControladorDepartamento.java
+++ mvcThJpa/src/main/java/com/example/mvc/controller/ControladorDepartamento.java
@@ -3,6 +3,7 @@ package com.example.mvc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +15,35 @@ import com.example.mvc.entities.Departamento;
 @Controller
 @RequestMapping("/departamento")
 public class ControladorDepartamento {
-	
+
 	@Autowired
 	ServicioDepartamento servicio;
 
+	Integer id;
+
 	@PostMapping
-	public String verDepartamento(@RequestParam Integer id,Model model) throws ServicioException {
-		
+	public String verDepartamento(@RequestParam Integer id, Model model) throws ServicioException {
+
+		this.id = id;
+
 		Departamento departamento = servicio.conseguirDepartamento(id);
-		
-		model.addAttribute("departamento",departamento);
-		
+
+		model.addAttribute("departamento", departamento);
+
 		return "departamento";
-		
+
 	}
-	
+
+	@GetMapping("/m")
+	public String verModificacionDepartamento(Model model) throws ServicioException {
+
+
+		Departamento departamento = servicio.conseguirDepartamento(id);
+
+		model.addAttribute(departamento);
+		
+		return "modificaciondepartamento";
+
+	}
+
 }
